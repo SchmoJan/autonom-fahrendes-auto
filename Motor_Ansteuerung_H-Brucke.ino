18387ac5d612e1585dcf98981fa7928aedf88d21
SchmoJan <56161306+SchmoJan@users.noreply.github.com>
2020-01-21 14:31:41 +0100

    Add files via upload

---
+++ Motor_Ansteuerung_H-Brucke.ino
@@ -0,0 +1,30 @@
+#define POTENSIO_PIN  0 // Pin Potentiometer A0
+#define RPWM_PIN      5 // Digital Pin 5 Arduino Arduino
+#define LPWM_PIN      6 // Digital Pin 6 Arduino Arduino
+
+void setup()
+{
+pinMode(RPWM_PIN, OUTPUT);
+pinMode(LPWM_PIN, OUTPUT);
+}
+
+void loop()
+{
+// sensor value range 0 to 1023
+int sensorValue = analogRead(POTENSIO_PIN);
+
+// reverse rotation
+if (sensorValue < 512)
+{
+int reversePWM = -(sensorValue - 511) / 2;
+analogWrite(LPWM_PIN, 0);
+analogWrite(RPWM_PIN, reversePWM);
+}
+
+else{
+// forward rotation
+int forwardPWM = (sensorValue - 512) / 2;
+analogWrite(LPWM_PIN, forwardPWM);
+analogWrite(RPWM_PIN, 0);
+}
+}
